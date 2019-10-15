77d5ec5d2fd9914d87daff24c5d68e8ae4a89524
Renato Toth <renatomolinat@gmail.com>
2019-10-15 13:20:25 -0700

    adding breaklines to improve readbility

--- index.js
+++ index.js
@@ -29,22 +29,27 @@ server.get('/projects', countRequests, (request, response) => {
 
 server.post('/projects', countRequests, (request, response) => {
   const { id, title } = request.body;
+
   projects.push({ id, title, tasks: [] });
   return response.json(projects);
 });
 
 server.delete('/projects/:id', countRequests, checkProjectExists, (request, response) => {
   const { id } = request.params;
+
   projectIndex = projects.findIndex((project) => project.id == id);
   projects.splice(projectIndex, 1);
+
   return response.json(projects);
 });
 
 server.put('/projects/:id', countRequests, checkProjectExists, (request, response) => {
   const { id } = request.params;
   const { title } = request.body;
+
   project = projects.find((project) => project.id == id);
   project.title = title;
+
   return response.json(projects);
 });
 
@@ -55,6 +60,7 @@ server.post('/projects/:id/tasks', countRequests, checkProjectExists, (request, 
 
   project = projects.find((project) => project.id == id);
   project.tasks = tasks;
+
   return response.json(projects);
 });
 
